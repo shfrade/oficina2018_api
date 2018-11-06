5252522cc6a5148e83f22c7c50ddf6cdeb698a48
Stefan Henrique Frade <stefan.henrique@outlook.com>
2018-11-06 19:40:53 -0300

    Requirements.txt

--- oficina_api/urls.py
+++ oficina_api/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include,re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('produtos.urls'))
 ]
